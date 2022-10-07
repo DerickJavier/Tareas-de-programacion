96aa413e8d9a0e63b8403cf5a847886c316784f1
DerickJavier25 <113707507+DerickJavier@users.noreply.github.com>
2022-10-06 21:03:48 -0400

    Ejemplo1

---
+++ main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+printf_s
+"Chiles morita rellenos
+con salsa de nuez\n");
+
+printf(150 g de chiles morita (unos 20).
+2 cucharadas de aceite.
+12 dientes de ajo.
+1 cebolla cortada en aros finos.
+2 tazas de vinagre de vino tinto.
+Sal.
+10 granos de pimienta negra.
+11/2 cucharadas de orégano seco.
+185 g de piloncillo rallado.
+Relleno:
+1 cucharada de aceite.
+1/2 cebolla finamente picada.
+2 dientes de ajo finamente picados.
+1/2 taza (125 g) de tomate finamente picado.)
+150 g de chiles morita (unos 20).
+
+
