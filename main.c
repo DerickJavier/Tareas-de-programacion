fd0e35c71ef87b578edd2c8d46e352a5ad84dfc9
DerickJavier25 <113707507+DerickJavier@users.noreply.github.com>
2022-10-06 21:28:56 -0400

    ej1

--- main.c
+++ main.c
@@ -1,20 +1,3 @@
-#include <stdio.h>
-#include <stdlib.h>
 
-int main()
-{
-    printf("Problemas y algoritmos\n");
-    return 0;
-}
-printf_s 1/4 taza; 30 g de almendras peladas y picadas/n";
- int 1/4 taza 30 g de uvas pasas sin semillas/n";
-int 1 pechuga entera de pollo cocida y finamente desmenuzada/n";
-int 1 cucharadita de sal/n";
- int 1/2cucharada de pimienta recién molida.
-Salsa/n;
-int 2 huevos, separadas las claras de las yemas/n;
-int 3/4 taza (90 g) de harina/n;
-int Aceite para freír/n;
-int 3/4 taza (90 g) de nueces/n;
-int 1 taza de crema de leche espesa, no azucarada/n;
-printf
+
+
