c790584cdcd4d14270161dcd635af708498fe70b
DerickJavier25 <113707507+DerickJavier@users.noreply.github.com>
2022-10-14 22:53:48 -0400

    Add files via upload

--- main.c
+++ main.c
@@ -1,71 +1,13 @@
 #include <stdio.h>
-#include <stdlib.h>
-
-
-
-int main()
+/* Aplicación de operadores. */
+void main(void)
 {
-    float entrada = 0.0;
-    float salida = 0.0;
-    int fin = 1;
-
-
-
-    while(fin != 0)
-    {
-        do
-        {
-            printf("Programa Calculo itbis\n");
-            printf("Ingrese el Costo = ");
-            scanf("%f",&entrada);
-
-
-
-            if(entrada < 0.0)
-            {
-                printf("\nEl precio es negativo, intente de nuevo");
-            }
-
-
-
-            if(entrada == 0)
-            {
-                fin = 0;
-                printf("\nFin de Programa!\n");
-            }
-
-
-
-        }while(entrada < 0);
-
-
-
-        if(entrada > 0)
-        {
-            //calculo
-            //salida = entrada + (entrada * 0.18);
-            salida = entrada + ((entrada * 18) / 100);
-
-
-
-            printf("\n\nEl itebis es: %f", entrada*0.18);
-            printf("\n\nEl Precio es: %f", salida);
-            printf("\n\n");
-
-
-
-        }
-
-
-
-
-
-    }
-
-
-
-
-
-    return 0;
+int i = 15, j, k, l;
+j = (15 > i--) > (14 < ++i);
+printf(“\nEl valor de j es: %d”, j);
+k = ! (‘b’ != ‘d’) > (!i - 1);
+printf(“\nEl valor de k es: %d”, k);
+l = (! (34 > (70 % 2))  0);
+printf(“\nEl valor de l es: %d”, l);
 }
-
+}
