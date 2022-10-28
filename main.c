61bb350bba1cbd1ca6e497c511f7c52ccd6b4b67
DerickJavier25 <113707507+DerickJavier@users.noreply.github.com>
2022-10-28 10:00:35 -0400

    Add files via upload
    Ejercicio pag 29

--- main.c
+++ main.c
@@ -1,16 +1,13 @@
 #include <stdio.h>
-/* Promedio de sueldos.
-El programa, al recibir como datos seis sueldos de un empleado, calcula tanto el
-➥ingreso total como el promedio mensual.
-CLA: variable de tipo entero.
-SU1, SU2, SU3, SU4, SU5, SU6, ING, PRO: variables de tipo real. */
+/* Superficie del triángulo.
+El programa, al recibir como datos la base y la altura de un triángulo,
+➥calcula su superficie.
+BAS, ALT y SUP: variables de tipo real. */
 void main (void)
 {
-int CLA;
-float SU1, SU2, SU3, SU4, SU5, SU6, ING, PRO;
-printf(“Ingrese la clave del empleado y los 6 sueldos: \n”);
-scanf(“%d %f %f %f %f %f %f”, &CLA, &SU1, &SU2, &SU3, &SU4, &SU5, &SU6);
-ING = (SU1 + SU2 + SU3 + SU4 + SU5 + SU6);
-PRO = ING / 6;
-printf(“\n %d %5.2f %5.2f”, CLA, ING, PRO);
+float BAS, ALT, SUP;
+printf(“Ingrese la base y la altura del triángulo: “);
+scanf(“%f %f”, &BAS, &ALT);
+SUP = BAS * ALT / 2;
+printf(“\nLa superficie del triángulo es: %5.2f”, SUP);
 }
