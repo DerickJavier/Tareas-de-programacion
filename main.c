170392bfa52d594272ceefffb90879ea5221ac53
DerickJavier25 <113707507+DerickJavier@users.noreply.github.com>
2022-10-29 00:26:30 -0400

    Add files via upload
    Ejercicio 2.5

--- main.c
+++ main.c
@@ -1,18 +1,33 @@
 #include <stdio.h>
+#include <math.h>
+/* Funcion matematica.
+El programa obtiene el resultado de una funcion.
 
-/* incremento de precio.
-El programa, al recibir como dato el precio de un producto, incrementa al
-➥mismo 11% si es menor a 1500$ y 8% en caso contrario (mayor o igual).
+OP y T: variables de tipo entero.
+RES: variable de tipo real. */
 
-PRE y NPR: variables de tipo real. */
 void main(void)
 {
-float PRE, NPR;
-printf(“Ingrese el precio del producto: “);
-scanf(“%f”, &PRE);
-if (PRE < 1500)
-NPR = PRE * 1.11;
-else
-    NPR = PRE * 1.08;
-printf(“\nNuevo precio del producto: %8.2f”, NPR);
+    int OP, T;
+    float RES;
+    printf (" Ingrese la opcion del calculo y el valor entero: ");
+
+    scanf(" %d %d", &OP, &T);
+    switch(OP)
+    {
+        case 1: RES = T / 5;
+        break;
+        case 2: RES = pow(T,T);
+        /* La funcion POW esta definida en la biblioteca math.H */
+        break;
+        case 3:
+        case 4: RES = 6 * T/2;
+        break;
+        default: RES = 1;
+        break;
+
+    }
+    printf("\nResultado:         %7.2f", RES);
 }
+
+
