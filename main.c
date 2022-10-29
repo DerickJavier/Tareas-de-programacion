2d9860699e5285e8caec1cfdb42ced8f792a40ef
DerickJavier25 <113707507+DerickJavier@users.noreply.github.com>
2022-10-29 00:20:30 -0400

    Ejercicio 2.3
    Programa 2.3

--- main.c
+++ main.c
@@ -1,18 +1,19 @@
-
 #include <stdio.h>
-/* Incremento de precio.
-El programa, al recibir como dato el precio de un producto importado,
-➥incrementa 11% el mismo si éste es inferior a $1,500.
-PRE y NPR: variable de tipo real. */
+#include <stdlib.h>
+#include <stdio.h>
+/* Promedio curso.
+El programa, al recibir como dato el promedio de un alumno en un curso
+➥universitario, escribe aprobado si su promedio es mayor o igual a 6, o
+➥reprobado en caso contrario.
+PRO: variable de tipo real. */
 void main(void)
 {
-float PRE, NPR;
-printf(“ingrese el precio del producto: “);
-scanf(“%f”, &PRE);
-if (PRE > 1500)
-{
-NPR = PRE * 1.11;
-printf(“\nNuevo precio: %7.2f”,NPR);
+float PRO;
+printf(“Ingrese el promedio del alumno: “);
+scanf(“%f”, &PRO);
+if (PRO >= 6.0)
+printf(“\nAprobado”);
+else
+printf(“\nReprobado”);
 }
-
 
