265777c0413c4bc1e56952bcef61c5883798b63a
DerickJavier25 <113707507+DerickJavier@users.noreply.github.com>
2022-10-29 00:46:12 -0400

    Ejercicio 2.15
    Programa 2.15

--- main.c
+++ main.c
@@ -1,35 +1,34 @@
 #include <stdio.h>
-
-/* Teléfono.
-El programa, al recibir como datos la clave de la zona geográfica y el
-numero de segundos de una llamada telefónica, calcula el costo de la misma.
-CLA y TIE: variables de tipo entero.
+/* Spa.
+El programa, al recibir como datos el tipo de tratamiento, la edad y el
+numero de dias de internación de un cliente en un spa, calcula el costo
+total del tratamiento.
+TRA, EDA, DIA: variables de tipo entero.
 COS: variable de tipo real. */
 void main(void)
 {
-int CLA, TIE;
+int TRA, EDA, DIA;
 float COS;
-printf("Ingresa la clave y el tiempo: ");
-scanf("%d %d", &CLA, &TIE);
-
-switch(CLA)
+printf("Ingrese tipo de tratamiento, edad y días: ");
+scanf("%d %d %d", &TRA, &EDA, &DIA);
+switch(TRA)
 {
- case 1: COS = TIE * 0.13 / 60; break;
- case 2: COS = TIE * 0.11 / 60; break;
- case 5: COS = TIE * 0.22 / 60; break;
- case 6: COS = TIE * 0.19 / 60; break;
- case 7:
- case 9: COS = TIE * 0.17 / 60; break;
- case 10: COS = TIE * 0.20 / 60; break;
- case 15: COS = TIE * 0.39 / 60; break;
- case 20: COS = TIE * 0.28 / 60; break;
- default : COS = -1; break;
- if (COS != -1)
-printf("\n\nClave: %d\tTiempo: %d\tCosto: %6.2f", CLA, TIE, COS);
+case 1: COS = DIA * 2800; break;
+case 2: COS = DIA * 1950; break;
+case 3: COS = DIA * 2500; break;
+case 4: COS = DIA * 1150; break;
+default: COS = -1; break;
+}
+if (COS != -1)
+{
+if (EDA >= 60)
+COS = COS * 0.75;
 else
-printf("\nError en la clave");
-
- }
-
-
-
+if (EDA <= 25)
+COS = COS * 0.85;
+printf("\nClave tratamiento: %d\t Días: %d\t Costo total: %8.2f",
+TRA, DIA, COS);
+}
+else
+printf("\nLa clave del tratamiento es incorrecta");
+}
