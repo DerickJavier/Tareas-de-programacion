d8bfcf80587658e4389e01fb4e365b249bb06275
DerickJavier25 <113707507+DerickJavier@users.noreply.github.com>
2022-10-29 00:33:25 -0400

    Ejercicio 2.8
    Programa 2.8

--- main.c
+++ main.c
@@ -1,32 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-/* ventas descendentes.
-El programa, al recibir datos como tres valores que representa las ventas
-de los vendedores de una tienda de discos, escribe las ventas en
-orden descendente.
-
-P, S y R: variable de tipo real.       */
-
+/* Asistentes.
+El programa, al recibir como datos la matrícula, la carrera, el semestre
+y el promedio de un alumno de una universidad privada, determina si
+éste puede ser asistente de su carrera.
+MAT, CAR Y SEM: variables de tipo entero.
+PRO: variable de tipo real. */
 void main(void)
 {
-    float P, S, R;
-    printf("/nIngrse las ventas de los tres vendedores:   ");
-    scanf("%f %f %f", &P, &S, &R);
-    if (P > S)
-        if (P > R)
-        if ( S > R)
-        printf("/n/n El orden es P, S y R:   %8.2f %8.2f" , P, S, R);
-    else
-        printf("/n/n El orden es P, R y S: %8.2f %8.2f", P, R, S);
-    else
-    printf("\n\n El orden es R, P y S: %8.2f %8.2f %8.2f" , R, P, S);
-    else
-    if (S > R)
-        if (P > R)
-    printf("\n\n El orden es S, P y R: %8.2f %8.2f", S, R);
-    else
-        printf("\n\n El orden es S, R y P: %8.2f %8.2f, S, R, P");
-        else
-            printf ("\n\n El orden es R, S y P: %8.2f %8.2f %8.2f", R, S, P);
+    int MAT, CAR, SEM;
+float PRO;
+printf("Ingrese matrícula: ");
+scanf("%d", &MAT);
+printf("Ingrese carrera ( 1-Industrial 2-Telemática  3-Computación  4-Mecánica) : ");
+
+scanf("%d", &CAR);
+printf("Ingrese semestre: ");
+scanf("%d", &SEM);
+printf("Ingrese promedio: ");
+scanf("%f", &PRO);
+switch(CAR)
+{
+
+case 1: if (SEM >= 6 && PRO >= 8.5)
+printf("\n%d %d %5.2f", MAT, CAR, PRO);
+break;
+case 2: if (SEM >= 5 && PRO >= 9.0)
+printf("\n%d %d %5.2f", MAT, CAR, PRO);
+break;
+case 3: if (SEM >= 6 && PRO >= 8.8)
+printf("\n%d %d %5.2f", MAT, CAR, PRO);
+break;
+case 4: if (SEM >= 7 && PRO >= 9.0)
+printf("\n%d %d %5.2f", MAT, CAR, PRO);
+break;
+default: printf("\n Error en la carrera");
+break;
+ }
 }
+
+
