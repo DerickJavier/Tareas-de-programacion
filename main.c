42ca8c4bf0adec649377dd84c118893d037ce03f
DerickJavier25 <113707507+DerickJavier@users.noreply.github.com>
2022-10-29 00:32:15 -0400

    Ejercicio 2.7
    Programa 2.7

--- main.c
+++ main.c
@@ -1,28 +1,32 @@
 #include <stdio.h>
-/* Incremento de salario.
- El programa, al recibir como dato el nivel de un profesor, incrementa
- su salario en función de la tabla 2.3.
+#include <stdlib.h>
 
- NIV: variable de tipo entero.
- SAL: variable de tipo real. */
+/* ventas descendentes.
+El programa, al recibir datos como tres valores que representa las ventas
+de los vendedores de una tienda de discos, escribe las ventas en
+orden descendente.
 
- void main(void)
- {
-     float SAL;
-     int NIV;
-     printf (" Ingrese el nivel academico del profesor:   ");
-     scanf ("%d", &NIV);
-     printf("Ingrese el salario:    ");
-     scanf("%f",  SAL);
-     switch(NIV)
-     {
-         case 1: SAL = SAL * 1.0035;  break;
-         case 2: SAL = SAL * 1.0041;  break;
-         case 3: SAL = SAL * 1.0048;  break;
-         case 4: SAL = SAL * 1.0053;  break;
-     }
-     printf("\n\nNivel:  %d \tNuevo salario:  %8.2f" ,NIV, SAL);
+P, S y R: variable de tipo real.       */
 
-     }
-
-
+void main(void)
+{
+    float P, S, R;
+    printf("/nIngrse las ventas de los tres vendedores:   ");
+    scanf("%f %f %f", &P, &S, &R);
+    if (P > S)
+        if (P > R)
+        if ( S > R)
+        printf("/n/n El orden es P, S y R:   %8.2f %8.2f" , P, S, R);
+    else
+        printf("/n/n El orden es P, R y S: %8.2f %8.2f", P, R, S);
+    else
+    printf("\n\n El orden es R, P y S: %8.2f %8.2f %8.2f" , R, P, S);
+    else
+    if (S > R)
+        if (P > R)
+    printf("\n\n El orden es S, P y R: %8.2f %8.2f", S, R);
+    else
+        printf("\n\n El orden es S, R y P: %8.2f %8.2f, S, R, P");
+        else
+            printf ("\n\n El orden es R, S y P: %8.2f %8.2f %8.2f", R, S, P);
+}
