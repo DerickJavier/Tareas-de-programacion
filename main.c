df8cb212e4dbcfe9fb1c20001298471474a8496a
DerickJavier25 <113707507+DerickJavier@users.noreply.github.com>
2022-11-17 21:24:06 -0400

    Capitulo 5

--- main.c
+++ main.c
@@ -1,41 +1,25 @@
 #include <stdio.h>
-/* Parámetros y funciones. */
-int f1(void);
-int f2(void); /* Prototipos de funciones. */
-int f3(void);
-int f4(void);
-int K = 5;
+/* Números perfectos.
+El programa, al recibir como dato un número entero positivo como límite, obtiene
+los números perfectos que hay entre 1 y ese número, y además imprime cuántos números
+perfectos hay en el intervalo.
+I, J, NUM, SUM, C: variables de tipo entero. */
 void main(void)
 {
-int I;
-for (I = 1; I <= 4; I++)
+int I, J, NUM, SUM, C = 0;
+printf("\nIngrese el número límite: ");
+scanf("%d", &NUM);
+for (I = 1; I <= NUM; I++)
 {
-printf("\n\nEl resultado de la función f1 es: %d", f1());
-printf("\nEl resultado de la función f2 es: %d", f2());
-printf("\nEl resultado de la función f3 es: %d", f3());
-printf("\nEl resultado de la función f4 es: %d", f4());
+SUM = 0;
+for (J = 1; J <= (I / 2); J++)
+if ((I % J) == 0)
+SUM += J;
+if (SUM == I)
+{
+printf("\n%d es un número perfecto", I);
+C++;
 }
 }
-int f1(void)
-{
-(K *= K);
-return (K);
-}
-int f2(void)
-{
-int K = 3;
-K++;
-return (K);
-}
-int f3(void)
-{
-static int K = 6;
-K += 3;
-return (K);
-}
-int f4(void)
-{
-int K = 4;
-(K = "K + ::K");
-return (K);
+printf("\nEntre 1 y %d hay %d números perfectos", NUM, C);
 }
