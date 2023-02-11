ce9883ba98a40dbfa4eca93f56320c5e9326bb40
DerickJavier25 <113707507+DerickJavier@users.noreply.github.com>
2023-02-10 20:41:21 -0400

    Programa para calcular la edad

---
+++ Programa edad 1.2/Programa edad 1.2/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace Programa_edad_1._2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.btnCalcular = new System.Windows.Forms.TextBox();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(153, 47);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(289, 26);
+            this.dateTimePicker1.TabIndex = 0;
+            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.btnCalcular_TextChanged);
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(207, 111);
+            this.btnCalcular.Multiline = true;
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.ReadOnly = true;
+            this.btnCalcular.Size = new System.Drawing.Size(162, 57);
+            this.btnCalcular.TabIndex = 1;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.btnCalcular.TextChanged += new System.EventHandler(this.btnCalcular_TextChanged);
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Location = new System.Drawing.Point(269, 203);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(18, 20);
+            this.lblResultado.TabIndex = 2;
+            this.lblResultado.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.TextBox btnCalcular;
+        private System.Windows.Forms.Label lblResultado;
+    }
+}
+
---
+++ Programa edad 1.2/Programa edad 1.2/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Programa_edad_1._2
+{
+    public partial class Form1 : Form
+    {
+        DateTime fechaNaciemiento;
+        DateTime fechaActual; 
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_TextChanged(object sender, EventArgs e)
+        {
+            fechaActual = DateTime.Today;
+            fechaNaciemiento = dateTimePicker1.Value;
+
+            int result = fechaActual.Year - fechaNaciemiento.Year;
+            lblResultado.Text = result.ToString();
+
+        }
+    }
+}
